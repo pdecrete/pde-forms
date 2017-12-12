7eba46681af3ce6ed03a8a3ae7eaa36a8732527e
Stavros Papadakis <spapad@gmail.com>
2017-12-12 09:00:01 +0200

    Update about page

--- yii2/views/site/about.php
+++ yii2/views/site/about.php
@@ -14,4 +14,8 @@ $this->params['breadcrumbs'][] = $this->title;
         Διεύθυνσης Εκπαίδευσης Κρήτης. </p>
     <p class="text-center"><img src="http://www.pdekritis.gr/www/wp-content/uploads/2015/12/logo-6.jpg" class="img-responsive" width="1140" height="171" alt="ΠΔΕ Κρήτης"/></p>
     <p class="text-center"><a href="http://www.pdekritis.gr/" title="ΠΔΕ Κρήτης" class="btn btn-lg btn-primary">Επισκεφθείτε τον ιστότοπο</a></p>
+
+    <h2>Επικοινωνία</h2>
+    <p>Για διαθέσιμους τρόπους επικοινωνίας επισκεφθείτε τη <a href="http://www.pdekritis.gr/contact/" title="ΠΔΕ Κρήτης - Επικοινωνία">σελίδα επικοινωνίας της ΠΔΕ Κρήτης</a>. 
+        Για διορθώσεις, νέες λειτουργίες μπορείτε επίσης να δημιουργήσετε <a href="https://github.com/spapad/pde-forms/issues/new" title="Issues">νέο issue στη σελίδα του έργου</a>.</p>
 </div>
